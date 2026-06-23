d68cb8cffb668492bd31f17cf8d942c252ba8863
AcoYT <167049722+AcoYTMC@users.noreply.github.com>
2026-06-23 06:58:02 -0700

    Initial commit

---
+++ src/main/java/com/example/template/impl/TemplateClient.java
@@ -0,0 +1,12 @@
+package com.example.template.impl;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TemplateClient implements ClientModInitializer {
+	public void onInitializeClient() {
+		//
+	}
+}
